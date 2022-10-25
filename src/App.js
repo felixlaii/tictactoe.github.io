5c5df4ff73a2973992326471a47981b5d7eaaa18
Felix Lai <felix.lai@hotmail.com>
2022-10-25 11:46:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -89,22 +89,17 @@ function App() {
     }
   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+     <Board>
+       {squares.map((square, index) => 
+       <Square 
+       x={square==='x'?1:0}
+       o={square==='o'?1:0}
+       onClick={() => handleSquareClick(index)}
+       />)}
+     </Board>
+     
+   </main>
   );
 }
 
