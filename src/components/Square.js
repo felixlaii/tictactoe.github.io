84b1babbaffd98add24b1bd4e2c5063bfae742f2
Felix Lai <felix.lai@hotmail.com>
2022-10-31 15:41:13 -0400

    passed props

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,4 +1,6 @@
-function Square(props) {
+import React from "react";
+
+function Square({ clickedArray, handleClick }) {
   return (
     <div className={"square"} {...props}>
       {props.x ? "x" : props.o ? "o" : ""}
