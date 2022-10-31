e6700d3d55e295dad14f5a9215414a07e060a02e
Felix Lai <felix.lai@hotmail.com>
2022-10-31 15:58:23 -0400

    Update TicTacToe.js

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -6,7 +6,14 @@ const INITIAL = "";
 const X_PLAYER = "X";
 const O_PLAYER = "O";
 const winCombination = [
-    
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ]
 function TicTacToe() {
   return (
