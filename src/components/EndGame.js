da543db49d170a51d849ca6adc39760ab1d3b556
Felix Lai <felix.lai@hotmail.com>
2022-10-31 15:47:57 -0400

    Update EndGame.js

--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function EndGame() {
+function EndGame({ clearHistory, winCount, restartGame, player, draw }) {
   return (
-    <div>EndGame</div>
+    <div className="end-game-screen">
+        {!draw && <span className="win-text">{player ? "O WON" : "X WON"}</span>}
+        {draw && <span className="win-text">DRAW GAME</span>}
+    </div>
   )
 }
 
